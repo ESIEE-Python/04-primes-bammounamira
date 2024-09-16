41982e930accc85d1df871262d28a5c92ce1c47a
bammounamira <amira.bammoun@edu.esiee.fr>
2024-09-16 11:29:49 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-from math import sqrt
-
-#### Fonction secondaire
-
-
 def isprime(p):
+    if p<2:
+        return False
+    for i in range (2,p):
+        if p % i == 0:
+            return False
+    return True
 
-    # votre code ici
-
-    pass
-
-#### Fonction principale
-
-
-def main():
-
-    # vos appels à la fonction secondaire ici
-
-    for n in range(100):
-        if isprime(n):
-            print(n, end=", ")
-
-    print()
-
-
-if __name__ == "__main__":
-    main()
+# print(isprime(11))
